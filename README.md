e70d23aca646552ca2535dc42992fa2fd57a8ac0
MSlinky <mario.solano.sc@gmail.com>
2020-03-25 14:30:59 -0600

    initLib

---
+++ README.md
@@ -0,0 +1,16 @@
+# Belugapay ecommerce checkout gateway to woocommerce
+
+The current plugin is to make transactions using belugapay ecommerce api using woocommerce for wordpress.
+
+## Requeriments
+
+* PHP 7.2 >=
+* [Wordpress](https://es-mx.wordpress.org/download/) 5.3.2 >=
+* [WooCommerce](https://wordpress.org/plugins/woocommerce/) 3.8.1 >=
+* [Belugapay ecommerce](https://gitlab.com/belugapaydevs/belugapaybackend/checkout-gateway-woocommerce) API-KEY
+
+## [Tutorial](https://gitlab.com/belugapaydevs/belugapaybackend/belugapay-checkout-gateway-woocommerce/-/wikis/Documentation(ES)/Instalar-plugin)
+
+## Authors
+
+* MSlinky
---
+++ templates/cardPayment.php
@@ -0,0 +1,21 @@
+<div>
+  <div class="form-row form-row-wide">
+    <label for="cardNumber">Número de tarjeta<span class="required">*</span></label>
+    <input id="cardNumber" name="cardNumber" class="input-text" type="text" />
+  </div>
+
+  <div class="form-row form-row-wide">
+    <label for="expirationMonth">Mes de expiración<span class="required">*</span></label>
+    <input id="expirationMonth" name="expirationMonth" class="input-text" type="text" />
+  </div>
+
+  <div class="form-row form-row-wide">
+    <label for="expirationYear">Año de expiración<span class="required">*</span></label>
+    <input id="expirationYear" name="expirationYear" class="input-text" type="text" />
+  </div>
+
+  <div class="form-row form-row-wide">
+    <label for="cvv">CVV<span class="required">*</span></label>
+    <input id="cvv" name="cvv" class="input-text" type="text" />
+  </div>
+</div>
