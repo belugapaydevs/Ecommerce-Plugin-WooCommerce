70d46f83320e90e311e3ea217e1b7bdaba7350fa
Mario Solano <mario.solano.sc@gmail.com>
2020-05-15 14:07:49 -0500

    feature(logos): update logos

--- templates/cardPayment.php
+++ templates/cardPayment.php
@@ -10,11 +10,51 @@
     grid-template-columns: calc(50% - 15px) calc(50% - 15px);
     gap: 30px;
   }
+  .blp-card {
+    position: relative;
+  }
+  .blp-logos {
+    position: absolute;
+    top: 0px;
+    right: 0;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  .blp-logos .carnet {
+    width: auto;
+    height: 25px;
+    margin-right: 10px;
+  }
+  .blp-logos .visa {
+    width: auto;
+    height: 15px;
+    margin-right: 10px;
+  }
+  .blp-logos .mastercard {
+    width: auto;
+    height: 20px;
+    margin-right: 10px;
+  }
 </style>
 <div class="form-blp-woocommerce">
+
+  <div class="form-row form-row-wide blp-mb">
+    <label for="cardHolder">Nombre del tarjetahabiente<span class="required">*</span></label>
+    <input id="cardHolder" name="cardHolder" class="input-text" type="text"/>
+  </div>
+
   <div class="form-row form-row-wide blp-mb">
     <label for="cardNumber">Número de tarjeta<span class="required">*</span></label>
-    <input id="cardNumber" name="cardNumber" class="input-text" type="text" maxlength="16"/>
+    <div class="blp-card">
+      <input id="cardNumber" name="cardNumber" class="input-text" type="text" maxlength="16"/>
+      <div class="blp-logos">
+        <img class="visa" src="<?php echo plugin_dir_url( dirname( __FILE__ ) ) . 'includes/images/logo_Visa.svg'; ?>">
+        <img class="mastercard" src="<?php echo plugin_dir_url( dirname( __FILE__ ) ) . 'includes/images/logo_mastercard.svg'; ?>">
+        <img class="carnet" src="<?php echo plugin_dir_url( dirname( __FILE__ ) ) . 'includes/images/logo_carnet.svg'; ?>">
+      </div>
+    </div>
   </div>
 
   <div class="blp-container-expiration blp-mb">
